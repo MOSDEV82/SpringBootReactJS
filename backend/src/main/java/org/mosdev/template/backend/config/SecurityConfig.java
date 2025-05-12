4bcf3ac4a4efe81a2d0b255b7e56288125f20464
mosdev82 <toni.weber@mosdev.org>
2025-05-12 10:59:14 +0200

    Enhance security and configuration handling
    Introduced dynamic application URL sourcing via `application.properties` for improved flexibility in allowed CORS origins. Adjusted server settings, added headers compression, and refined project structure for better maintainability. Commented out unused dependencies and streamlined configuration definitions.

--- backend/src/main/java/org/mosdev/template/backend/config/SecurityConfig.java
+++ backend/src/main/java/org/mosdev/template/backend/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package org.mosdev.template.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -11,14 +14,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${application.url}")
+    private String applicationURL;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -45,9 +51,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(java.util.Arrays.asList(
-            "http://localhost:3000", 
-            "http://localhost:5173", 
-            "https://template.mosdev.org"
+            "http://localhost:3000",
+            "http://localhost:5173",
+            applicationURL
         ));
         configuration.setAllowedMethods(java.util.Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(java.util.Arrays.asList("authorization", "content-type", "x-auth-token"));
